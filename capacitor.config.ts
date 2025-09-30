e8082008861b43b8ddd08f3e950547c7ebc5e2f5
Carla Arauz <cparauz@gmaiaal.com>
2025-09-30 14:52:58 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-bd-api',
+  webDir: 'www'
+};
+
+export default config;
