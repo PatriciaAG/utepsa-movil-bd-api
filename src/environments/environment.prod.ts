7bfbf19a776ba45cb4ed32b14797b389006d88be
Carla Arauz <cparauz@gmaiaal.com>
2025-10-01 20:56:06 -0400

    Actualización agregando configuración de seguridad, rutas y autenticación

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAJXhp__pFN_cdShDTt111pE89HDTdfUlY",
+    authDomain: "practica-a485b.firebaseapp.com",
+    projectId: "practica-a485b",
+    storageBucket: "practica-a485b.firebasestorage.app",
+    messagingSenderId: "545717347244",
+    appId: "1:545717347244:web:a5ad67446e5144f2c39009"
+  }
 };
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,23 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp } from 'firebase/app';
+import { environment } from './environments/environment';
+import { provideAuth } from '@angular/fire/auth';
+import { getAuth } from 'firebase/auth';
+import { provideFirestore } from '@angular/fire/firestore';
+import { getFirestore } from 'firebase/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 });
